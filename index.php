85011e967d2c9fda61d6f83f4c777b4e81333c1c
Esther Lecina <estherlecina@gmail.com>
2021-02-10 18:13:16 +0100

    creada y añadida la branche del sprint 6

--- index.php
+++ index.php
@@ -1,64 +1,128 @@
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>calco wireframe</title>
-		<link rel="stylesheet" href="css/estilos.css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet"> 
-	</head>
-	<body>
-		<?php include 'header.php';?><!--El codi PHP que enllaça amb l'arxiu que conté el header--->
-		<nav>
-			<ul>
-				<li><a href="#">Primero</a></li>
-				<li><a href="#">Segundo</a></li>
-				<li><a href="#">Tercero</a></li>
-				<li><a href="#">Cuarto</a></li>
-				<li><a href="#">Quinto</a></li>
-			</ul>
-		</nav>
-		<aside>
-			<div id="dest1" class="destacado">
-				<h3>Primer destacat</h3>
-				<input style="cursor: pointer" type="button" value="accedir" onclick="window.location.href='#'">
-				<!--s'han creat botons que funcionen mitjançant javascript--->
-				<!--les imatges que apareixerán a sota el destacat son decoratives, així que s'han posat mitjançant css--->
-			</div>
-			<div id="dest2" class="destacado">
-				<h3>Segon destacat</h3>
-				<input style="cursor: pointer" type="button" value="accedir" onclick="window.location.href='#'">
-			</div>
-			<div id="dest3" class="destacado">
-				<h3>Tercer destacat</h3>
-				<input style="cursor: pointer" type="button" value="accedir" onclick="window.location.href='#'">
-			</div>
-		</aside>
-		<article>
-			<div class="principal">
-				<div class="modulo">
-					<img src="img/agricultur.jpg" alt="imagen de agricultura" width="180">
-					<h2>Artículo</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque odio at tellus blandit, at facilisis erat ornare. Aenean eu eros congue dui tincidunt pulvinar nec non ipsum.</p>
-					<input class="leermas" style="cursor: pointer" type="button" value="Veure més" onclick="window.location.href='#'">
-				</div>
-				<div class="modulo">
-					<img src="img/hornet.jpg" alt="imagen de una abeja" width="180">
-					<h2>Otro artículo más</h2>
-					<p>Ut pellentesque odio at tellus blandit, at facilisis erat ornare. Aenean eu eros congue dui tincidunt pulvinar nec non ipsum. Nam gravida dolor in ipsum gravida pulvinar.</p>
-					<input class="leermas" style="cursor: pointer" type="button" value="Veure més" onclick="window.location.href='#'">
-				</div>
-				<div class="modulo">
-					<img src="img/lens.jpg" alt="imagen de una lente" width="180">
-					<h2>Y otro más</h2>
-					<p>Ut pellentesque odio at tellus blandit, at facilisis erat ornare. Aenean eu eros congue dui tincidunt pulvinar nec non ipsum.</p>
-					<input class="leermas" style="cursor: pointer" type="button" value="Veure més" onclick="window.location.href='#'">
-				</div>
-			</div>
-		</article>
-		<?php include 'footer.php';?>
+    <head>
+        <meta charset="UTF-8"></meta>
+        <title>PIZZERÍA SELENITA</title>
+        <link rel="stylesheet" href="css/estilos.css">
+        <script src="js/main.js"></script>
+    </head>
+    <body>
 
-	</body>
+        <?php
+        /*         * *************NIVEL 1: EJERCICIO 1: FUNCION resta()****** */
+
+        function resta($n1, $n2) {
+            $resultat = $n1 - $n2;
+            return $resultat;
+        }
+
+        $num = resta(4, 2);
+        echo $num;
+        echo "<br>";
+
+        /*         * *************NIVEL 1: EJERCICIOS 2 y 3: FUNCION espar()****** */
+
+        function espar($victimilla) {
+            $rest = $victimilla % 2;
+            if ($rest == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        $bool = espar($num);//cogemos el número resultado dela resta del ejercicio anterior
+        if ($bool) {
+            echo "Numero par";
+        } else {
+            echo "Numero impar";
+        }
+
+        echo "<br>";
+        echo "<br>";
+
+        /****NIVEL 1: EJERCICIO 4: cuenta hasta 10***/
+
+        for ($x = 1; $x <= 10; $x++) {
+            echo $x . " ";
+        }
+        echo "<br>";
+        echo "<br>";
+
+        /*************NIVEL 2: EJERCICIO 1 cuenta10 de 2 en 2***********/
+
+        function cuentatrampa() {
+            for ($x = 2; $x <= 10; $x += 2) {
+                echo $x . " ";
+            }
+        }
+
+        cuentatrampa();
+        echo "<br>";
+        echo "<br>";
+
+
+        /*         * *NIVEL 2: EJERCICIOS 2 Y 3 cuenta hasta $x de 2 en 2*** */
+
+        function cuentatrampa2($valor) {
+            if (!is_numeric($valor)) {//comprueba que el parámetro recibido sea numérico
+                $valor = 10;
+            }
+            for ($x = 0; $x <= $valor; $x += 2) {
+                echo $x . " ";
+            }
+        }
+
+        cuentatrampa2("20"); //aunque es una cadena se envía y funciona
+        echo "<br>";
+        echo "<br>";
+
+        /*         * *NIVEL 3: EJERCICIO 1 anys_olimpics()* */
+
+        echo "<b>Anys olímpics: </b><br>";
+        for ($x = 1960; $x <= 2016; $x += 4) {
+            echo $x . " - ";
+        }
+        echo "<br>";
+        echo "<br>";
+
+        /*         * ***NIVEL 3: EJERCICIO 2 La tienda***** */
+
+        $chicles = 1; //valen 0.50
+        $chocolates = 2; //valen 1
+        $caramelos = 1; //valen 1.50
+
+        function calculachuches($chic, $choc, $car) {
+            $total = $chic * 0.5 + $choc + $car * 1.5;
+            echo "Has cogido " . $chic . " chicles, " . $choc . " chocolates y " . $car . " caramelos. <br> En total, todo suma " . $total . " euros.";
+        }
+
+        calculachuches($chicles, $chocolates, $caramelos);
+        echo "<br>";
+        echo "<br>";
+
+        //*******NIVEL 3: EJERCICIO 3 criba de Erastótenes*******//
+
+        $rango = 64;
+        for ($i = 2; $i < $rango; $i++) {
+            $numeros[$i] = true;
+        }
+
+        $numeros[2] = true;//marcamos el primer primo como tal
+
+        for ($n = 2; $n < $rango; $n++) {
+            if ($numeros[$n]) {
+                for ($i = $n * $n; $i < $rango; $i += $n) {
+                    $numeros[$i] = false;//se marcan los que no son primos
+                }
+            }
+        }
+        echo "<b>Los números primos son:</b><br>";
+        for ($n = 2; $n < $rango; $n++) {
+            if ($numeros[$n]) {
+                echo $n . " ";//escribimos los que no estan marcados
+            }
+        }
+        ?>
+    </body>
 </html>
