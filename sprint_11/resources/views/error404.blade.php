225ad8e302ddda4b7aefd0e2ddb2626fe40669d1
Esther Lecina <estherlecina@gmail.com>
2021-04-06 17:06:28 +0200

    primer commit

--- sprint_11/resources/views/error404.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-@section('content')
-
-<h1>Error 404</h1>
-<p class="lead">Pues eso...</p>
-<a class="btn btn-lg btn-primary" href="{{ url('/') }}" role="button">Volver a inicio</a>
-
-@endsection
