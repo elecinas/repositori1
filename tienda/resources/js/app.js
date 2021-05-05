434aeeeb863cc2c04e4fe9cfe6790810b6814523
Esther Lecina <estherlecina@gmail.com>
2021-05-05 19:04:19 +0200

    intentando que furule VUE D:

--- tienda/resources/js/app.js
+++ tienda/resources/js/app.js
@@ -1,3 +1,15 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+/*IMPORTANDO VUE*/
+
+import { createApp } from 'vue';
+import ExampleComponent from  './components/ExampleComponent'
+
+        createApp({
+            components: {
+                ExampleComponent,
+            }
+        }).mount('#app');
+
--- tienda/resources/views/layout.blade.php
+++ tienda/resources/views/layout.blade.php
@@ -3,11 +3,17 @@
     @extends('extractos.head')
     <body class="animsition">
         <div class="page-wrapper">
-        @extends('extractos.nav')
-        
-                @yield('content')
-           
-        @extends('extractos.footbootstrap')
+            @extends('extractos.nav')
+
+            @yield('content')
+
+            @extends('extractos.footbootstrap')
         </div>
+        {{-- VUE --}}
+        <div id="app">
+            <example-component></example-component>
+        </div>
+        <script src="{{ mix('js/app.js') }}"></script>
+        {{-- ENDVUE --}}
     </body>
 </html>
